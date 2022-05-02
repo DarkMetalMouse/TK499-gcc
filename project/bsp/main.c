1398dc13950b3b705c8dc7e65e99d4b1d29010c8
DarkMetalMouse <51059131+DarkMetalMouse@users.noreply.github.com>
2022-05-02 17:04:12 +0300

    initial commit

---
+++ project/bsp/main.c
@@ -0,0 +1,49 @@
+/****************************************Copyright (c)****************************************************
+** 
+**                                      
+**
+**--------------File Info---------------------------------------------------------------------------------
+** File name:			main.c
+** modified Date:  		2017-6-20
+** Last Version:		V0.1
+** Descriptions:		  main 函数调用
+**
+** 好钜润科技，芯片事业部----深圳龙华应用分部
+*********************************************************************************************************/
+#include "HAL_conf.h"
+#include "sys.h"
+/********************************************************************************************************
+**函数信息 ：int main (void)                          
+**功能描述 ：
+**输入参数 ：
+**输出参数 ：
+********************************************************************************************************/
+
+int main(void)
+{
+	uint32_t  i;
+	GPIO_InitTypeDef GPIO_InitStructure;//定义GPIO初始化结构体变量
+	RemapVtorTable();
+	SystemClk_HSEInit(RCC_PLLMul_20);//启动PLL时钟，12MHz*20=240MHz
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOD, ENABLE);
+
+	//配置连接LED的GPIO为推挽输出模式
+	GPIO_InitStructure.GPIO_Pin  =  GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+
+	while(1)//无限循环 
+	{
+		GPIO_SetBits(GPIOD, GPIO_Pin_8); //PC8输出高电平，点亮LED
+		for(i=0;i<2000000;i++);//延时
+		
+		GPIO_ResetBits(GPIOD, GPIO_Pin_8);//PD8输出低电平，熄灭LED
+		for(i=0;i<2000000;i++);//延时
+	}
+
+}
+
+
+
